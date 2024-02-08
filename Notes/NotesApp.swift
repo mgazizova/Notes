1e41359ab0ad138a8973f2f2e362f97ae8908ddd
Mariia Gazizova <mgazizova5@gmail.com>
2024-02-08 23:24:25 +0100

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Мария Газизова on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
